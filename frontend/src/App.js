09f09c0b592f4e6573cea15467285c2ec9b0199e
yihsin9111 <b10901121@ntu.edu.tw>
2022-12-04 23:19:12 +0800

    add antd but react not found ?

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import "antd/dist/antd.css"; 
+//import "antd/dist/antd.css"; 
 import * as React from 'react';
 import FirstPage from "./containers/firstPage";
 
