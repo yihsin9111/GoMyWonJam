1f025a8c5d6d878f58330a76f41f763d8c3234c2
yihsin9111 <b10901121@ntu.edu.tw>
2022-12-13 01:34:36 +0800

    setup backend

---
+++ backend/src/models/Product.js
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose'
+
+const Schema = mongoose.Schema
+
+const ProductSchema = Schema({
+    name:       { type: String, required: true },
+    URL:        { type: String, required: true },
+    deadline:   { type: String, required: true },
+    bought:     { type: Number, required: true },
+    sold:       { type: Number, required: true },
+    product_type: { type: String, required: true }
+}, {
+    collection: 'Product',
+    timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' }
+}) 
+
+const exportSchema = mongoose.model('Product', ProductSchema)
+
+export default ProductSchema
