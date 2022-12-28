016665a69309ad9e45bd23ccac95d553a48c2380
lizhigugu <sandyingg@gmail.com>
2022-12-29 03:38:32 +0800

    cartlist.js added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
-import "antd/dist/antd.css"; 
-import * as React from 'react';
+//import "antd/dist/antd.css"; 
+import { useState } from "react";
 
 // Container Import
 import MainPage from './containers/MainPage';
 import TestPage from "./containers/testPage";
+import CartList from './components/CartList';
 
 // Bar Component Import
 import NavBar from "./components/BarComponent/NavBar";
 import {Main, DrawerHeader} from "./components/BarComponent/barPositionHandler";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   // set state
@@ -23,6 +25,7 @@ function App() {
         <Routes>
           <Route path="/" element={<MainPage />} />
           <Route path="/test" element={<TestPage />} />
+          <Route path="/cartlist" element={<CartList />} />
         </Routes>
         </DrawerHeader>
       </Main>
