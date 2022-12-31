daac6efd0c69e41198206bb3e3fa7ab2b59aa186
lizhigugu <sandyingg@gmail.com>
2022-12-31 16:44:46 +0800

    checkPage added

--- frontend/src/containers/CheckPage.js
+++ frontend/src/containers/CheckPage.js
@@ -2,6 +2,21 @@
 
 //mui import 
 import { Card, CardContent, Typography } from "@mui/material";
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import MenuItem from '@mui/material/MenuItem';
+import TextField from '@mui/material/TextField'
+import  Divider  from '@mui/material/Divider';
+
+//component import 
+import Receipt from '../components/PersonalComponent/Receipt';
+
+//test Data
+import Bills from "../test datas/Bills"
+import { Grid } from 'antd';
+import Item from 'antd/es/list/Item';
+import { isEnumType } from 'graphql';
+import Input from 'antd/es/input/Input';
 
 //functional component
 const CheckPage = () => {
